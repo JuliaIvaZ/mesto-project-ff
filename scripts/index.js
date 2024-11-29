f676bee704a2043bc582632f6650be31d62f8ccc
Julia Zagaevskaya <u.zagaevskaya@gmail.com>
2024-11-29 10:55:37 +0300

    Creating vriables

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template');
 // @todo: DOM узлы
-
+const cardImage = cardTemplate.querySelector('.card__image');
+const cardDeleteButton = cardTemplate.querySelector('.card__delete-button');
+const cardLikeButton = cardTemplate.querySelector('.card__like-button');
+const cardDescription = cardTemplate.querySelector('.card__description');
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
