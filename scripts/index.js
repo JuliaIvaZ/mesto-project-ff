a860fe853fcbd595ba12ebc632fb43fa7c1d787d
Julia Zagaevskaya <u.zagaevskaya@gmail.com>
2024-11-30 00:15:51 +0300

    Правки по ревью_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,43 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const defaultCard = document.querySelector('#card-template').content.querySelector('.card');
+
 // @todo: DOM узлы
-const card = cardTemplate.querySelector('.card');
-
-const cardDeleteButton = card.querySelector('.card__delete-button');
-const cardImage = card.querySelector('.card__image');
-const cardDescription = card.querySelector('.card__description');
-
-const cardTitle = cardDescription.querySelector('.card__title');
-const cardLikeButton = cardDescription.querySelector('.card__like-button');
-
 const placesList = document.querySelector('.places__list');
+let cardData = {};
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+    const card = defaultCard.cloneNode(true); 
 
-function createCard(cardImage, cardTitle, deleteCard) {
-    let cardContainer = document.createElement('li');
-    cardContainer.classList.add('card');
+    const cardImage = card.querySelector('.card__image');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
 
-    let containerImage = document.createElement('img');
-    containerImage.classList.add('card__image');
-    containerImage.src = cardImage.svg;
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', () => deleteCard(card));
 
-    let containerButton = document.createElement('button');
-    containerButton.classList.add('card__delete-button');
-    containerButton.addEventListener('click', deleteCard);
+    const cardDescription = card.querySelector('.card__description');
 
-    let containerDescription = document.createElement('div');
-    containerDescription.classList.add('card__description');
+    const cardTitle = cardDescription.querySelector('.card__title');
+    cardTitle.textContent = cardData.name;
 
-    let containerTitle = document.createElement('h2');
-    containerTitle.classList.add('card__title');
-    containerTitle.textContent = cardTitle.h2;
+    const cardLikeButton = cardDescription.querySelector('.card__like-button');
 
-    let containerLikeButton = document.createElement('button');
-    containerLikeButton.classList.add('card__like-button');
+    cardDescription.append(cardTitle, cardLikeButton);
+    card.append(cardImage, cardDeleteButton, cardDescription);
 
-    containerDescription.append(containerTitle, containerLikeButton);
-    cardContainer.append(containerImage, containerButton, containerDescription);
-
-    cardDeleteButton.addEventListener('click', deleteCard);
-
-    placesList.append(cardContainer);
+    return card; 
 }
 
 // @todo: Функция удаления карточки
-
-function deleteCard() {
-    const deleteList = document.querySelector('.card');
-    deleteList.remove();
+function deleteCard(card) {
+    card.remove();
 }
+
 // @todo: Вывести карточки на страницу
-
-for (let i = 0; i < initialCards.length; i++) {
-    cardImage.svg = initialCards[i].link;
-    cardTitle.h2 = initialCards[i].name;
-    createCard(cardImage, cardTitle, deleteCard);
-}
+initialCards.forEach((item) => {
+    cardData.link = item.link;
+    cardData.name = item.name;
+    const cardElement = createCard(cardData, deleteCard);
+    placesList.append(cardElement);
+});
