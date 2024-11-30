7d57445128e95f17105bde2810d89a94dfbcab6b
Julia Zagaevskaya <u.zagaevskaya@gmail.com>
2024-11-30 22:55:15 +0300

    Правки по ревью_2

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,14 @@ const defaultCard = document.querySelector('#card-template').content.querySelect
 
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
-let cardData = {};
 
 // @todo: Функция создания карточки
-function createCard(cardData, deleteCard) {
+function createCard(item, deleteCard) {
     const card = defaultCard.cloneNode(true); 
 
     const cardImage = card.querySelector('.card__image');
-    cardImage.src = cardData.link;
-    cardImage.alt = cardData.name;
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
 
     const cardDeleteButton = card.querySelector('.card__delete-button');
     cardDeleteButton.addEventListener('click', () => deleteCard(card));
@@ -19,12 +18,9 @@ function createCard(cardData, deleteCard) {
     const cardDescription = card.querySelector('.card__description');
 
     const cardTitle = cardDescription.querySelector('.card__title');
-    cardTitle.textContent = cardData.name;
+    cardTitle.textContent = item.name;
 
     const cardLikeButton = cardDescription.querySelector('.card__like-button');
-
-    cardDescription.append(cardTitle, cardLikeButton);
-    card.append(cardImage, cardDeleteButton, cardDescription);
 
     return card; 
 }
@@ -36,8 +32,6 @@ function deleteCard(card) {
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((item) => {
-    cardData.link = item.link;
-    cardData.name = item.name;
-    const cardElement = createCard(cardData, deleteCard);
+    const cardElement = createCard(item, deleteCard);
     placesList.append(cardElement);
 });
