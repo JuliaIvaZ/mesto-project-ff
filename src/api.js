8d3c6e91799dae9a422d3d1dfb49b07ea18bd544
Julia Zagaevskaya <u.zagaevskaya@gmail.com>
2025-03-12 15:08:50 +0300

    Правки

--- src/api.js
+++ src/api.js
@@ -1,112 +1,94 @@
-//import { get } from "jquery";
+const BASE_URL = 'https://nomoreparties.co/v1/wff-cohort-33';
+const get_headers = {
+    authorization: '07069f22-afde-49a4-88de-b3e1ba6e8b6f',
+    'Content-Type': 'application/json'
+};
 
-//let userName, userAbout, userAvatar, userId, cohort;
+function getData (res) {
+    if (!res.ok)
+        return Promise.reject(`Ошибка: ${res.status}`)
+    return res.json();
+};
 
+// Данные пользователя
 const getUserInfo = () => {
-    return fetch ('https://nomoreparties.co/v1/wff-cohort-33/users/me', {
+    return fetch (`${BASE_URL}/users/me`, {
         method: 'GET',
-        headers: {
-            authorization: '07069f22-afde-49a4-88de-b3e1ba6e8b6f',
-            'Content-Type': 'application/json'
-        }
+        headers: get_headers
     })
-    .then(res => { 
-        if (!res.ok) {
-            throw new Error(`Ошибка: ${res.status}`);
-        }
-        return res.json() })
-    .then(data => {
-        console.log('Данные пользователя_00:  ', data);
-        return data;
-    })
-    .catch(err => {
-        console.log('Ошибка при получении данных:', err);
-        throw err;
-    });
+    .then ((data) => {
+        console.log(data)})
+    .then(getData)
+   
 };
 
+// Получение карточек
 const getCards = () => {
-    return fetch ('https://nomoreparties.co/v1/wff-cohort-33/cards', {
+    return fetch (`${BASE_URL}/cards`, {
         method: 'GET',
-        headers: {
-            authorization: '07069f22-afde-49a4-88de-b3e1ba6e8b6f',
-            'Content-Type': 'application/json'
-        }
+        headers: get_headers
     })
-    .then(res => { 
-        if (!res.ok) {
-            throw new Error(`Ошибка: ${res.status}`);
-        }
-        return res.json() })
-    .then(data => {
-            console.log('Данные пользователя:', data); // Проверяем данные
-            return data;
-        })
-    .catch(err => {
-        console.log('Ошибка при получении данных:', err);
-        throw err;
-    });
+    .then(getData)
 };
 
+// Редактирование профиля
 const setUserInfo = (userName, userAbout) => {
-    return fetch ('https://nomoreparties.co/v1/wff-cohort-33/users/me', {
+    return fetch (`${BASE_URL}/users/me`, {
         method: 'PATCH',
-        headers: {
-            authorization: '07069f22-afde-49a4-88de-b3e1ba6e8b6f',
-            'Content-Type': 'application/json'
-        },
+        headers: get_headers,
         body: JSON.stringify({
             name: userName,
             about: userAbout
         })
-    });
-
+    })
+    .then ((data) => {
+        console.log(data)})
+    .then(getData);
 };
 
+// Создание новой карточки
 const setNewCard = (cardName, cardLink) => {
-    return fetch ('https://nomoreparties.co/v1/wff-cohort-33/cards', {
+    return fetch (`${BASE_URL}/cards`, {
         method: 'POST',
-        headers: {
-            authorization: '07069f22-afde-49a4-88de-b3e1ba6e8b6f',
-            'Content-Type': 'application/json'
-        },
+        headers: get_headers,
         body: JSON.stringify({
             name: cardName,
             link: cardLink
         })
     })
+    .then ((data) => {
+        console.log(data)})
+    .then(getData);
 };
 
-const setLike = (cardId) => {
-    return fetch (`https://nomoreparties.co/v1/wff-cohort-33/cards/likes/${cardId}`, {
-        method: 'PUT',
-        headers: {
-            authorization: '07069f22-afde-49a4-88de-b3e1ba6e8b6f'
-        }
-    })
-    .then ((res) => {
-        if (!res.ok) {
-            throw new Error(`Ошибка постановки лайка_1: ${res.status}`);
-        }
-        return res.json()
-    })
-    .catch (err => console.log(err));
-};
-
-const removeLike = (cardId) => {
-    return fetch (`https://nomoreparties.co/v1/wff-cohort-33/cards/likes/${cardId}`, {
+// Удаление своей карточки
+const deleteUserCard = (cardId) => {
+    return fetch (`${BASE_URL}/cards/${cardId}`, {
         method: 'DELETE',
-        headers: {
-            authorization: '07069f22-afde-49a4-88de-b3e1ba6e8b6f'
-        }
+        headers: get_headers
     })
-    .then ((res) => {
-        if (!res.ok) {
-            throw new Error(`Ошибка снятия лайка_1: ${res.status}`);
-        }
-        return res.json()
-    })
-    .catch (err => console.log(err));
+    .then(getData);
 };
 
-export { getUserInfo, getCards, setUserInfo, setNewCard, setLike, removeLike };
+// Постановка лайка
+const setLike = (cardId) => {
+    return fetch (`${BASE_URL}/cards/likes/${cardId}`, {
+        method: 'PUT',
+        headers: get_headers
+    })
+    .then(getData);
+};
+
+// Удаление лайка
+const removeLike = (cardId) => {
+    return fetch (`${BASE_URL}/cards/likes/${cardId}`, {
+        method: 'DELETE',
+        headers: get_headers
+    })
+    .then(getData)
+};
+
+export { getUserInfo, getCards, setUserInfo, setNewCard, deleteUserCard, setLike, removeLike };
+
+
+  
